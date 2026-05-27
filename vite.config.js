bb1b2dc0f8f0970f5f48f60b48c166379cc54794
apple <2435665508@qq.com>
2026-05-27 23:22:14 +0800

    3.1更新配置

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
+  // 部署 GitHub Pages 必须加的路径（改成你的仓库名）
+  base: "/test/",
+
   plugins: [vue()],
   resolve: {
     alias: { '@': '/src' }
